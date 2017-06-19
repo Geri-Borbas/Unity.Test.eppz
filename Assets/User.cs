8ca9a1f9421e2206eb5ed2c82de19ec9c6ceb09e
eppz <hello@eppz.eu>
2017-06-19 20:08:29 +0200

    0.0.9

---
+++ Assets/User.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Game
+{
+
+
+	public class User : MonoBehaviour
+	{
+
+
+		#if LIBRARY_IS_AVAILABLE
+		public string message = "LIBRARY_IS_AVAILABLE";
+		#endif
+	}
+}
